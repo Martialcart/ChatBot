83a5ce0b55ae374304881006af1d538f6910e67e
Jan_Olav_Berg <janolav91@hotmail.com>
2020-11-12 14:31:44 +0100

    finnished 3 wordify tests

--- src/test/java/ToolsTest.java
+++ src/test/java/ToolsTest.java
@@ -1,24 +1,35 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ToolsTest {
+    @BeforeAll
+    public void setup(){
+
+    }
 
     @Test
     public void WordifyRemoveSignsTest(){
-        Tools.wordify("a!b#q¤g%f&s/()=?}][{€}]");
+        String signs = "|§!\"#¤%&/()=?`^¨'*_-:~´´}][{€$£@";
+        String[] expected = {""};
+        String[] result = Tools.wordify(signs);
+        assertEquals(expected, result);
     }
 
     @Test
     public void WordifySentenceToWordsTest(){
-
-    }
-
-    @Test
-    public void WordifyRemoveSignsAnddivideTest(){
-
+        String sentence = "this is a test";
+        String[] expected = {"this", "is", "a", "test"};
+        String[] result = Tools.wordify(sentence);
+        assertEquals(expected, result);
     }
 
     @Test
     public void WordifyUpperToLowerCaseTest() {
-
+        String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        String[] expected = {"abcdefghijklmnopqrstuvwxyzæøå"};
+        String[] result = Tools.wordify(uppercase);
+        assertEquals(expected, result);
     }
 }
