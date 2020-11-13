43a7fb0fa6ba6823804215b6250fb4867c78fa3f
Jan_Olav_Berg <janolav91@hotmail.com>
2020-11-13 17:22:10 +0100

    chatbot appears to be working

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,6 +1,3 @@
-import com.sun.org.apache.xalan.internal.xsltc.dom.MultiValuedNodeHeapIterator;
-import com.sun.scenario.effect.impl.HeapImage;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,28 +9,59 @@ public class ChatBot {
     private Map<String, List<Integer>> word = new HashMap<String, List<Integer>>();
     private Scorer<Integer> sentenceScore = new Scorer<>();
     private Integer temp;
+    private Integer lastResponseIndex;
+    private final String WELCOME = "hi, I am chatbot, how are you doing?";
 
     public ChatBot() {
+        sentence.add(WELCOME);
     }
     public String send(String message) {
-        sentence.add(message);
-        log.addAll(Tools.wordify(message));
-        sentenceScore.reset();
+        recordMessage(message);
+        return findAnswer();
+    }
+    public String welcomeMessage() {
+        lastResponseIndex = 0;
+        return sentence.get(0);
+    }
 
-        for (int i = log.size(); 0 < i; i--) {
+    private String findAnswer() {
+        sentenceScore.reset();
+        for (int i = log.size()-1; 0 < i; i--) {
             if(word.containsKey(log.get(i))) {
                 for (Integer w :word.get(log.get(i))) {
-                   sentenceScore.add(w);
-                   temp = sentenceScore.winner();
-                   if(temp != null) {
-                       return sentence.get(temp);
-                   }
+                    sentenceScore.add(w);
+                    if(i%5 == 0) temp = sentenceScore.winner();
+                    if(temp != null) {
+                        lastResponseIndex = temp;
+                        return sentence.get(temp);
+                    }
                 }
             }
         }
-
-        return "";
+        return randomMessage();
     }
 
+    private String randomMessage() {
+        return sentence.get((int)(Math.random() * (sentence.size() - 1)));
+    }
 
+    private void recordMessage(String message) {
+        linkPreviousResponse(message);
+        sentence.add(message);
+        log.addAll(Tools.wordify(message));
+    }
+
+    private void linkPreviousResponse(String message) {
+        if(sentence.contains(message)) {
+            temp = sentence.indexOf(message);
+            for(String w: Tools.wordify(sentence.get(lastResponseIndex))) {
+                if(word.containsKey(w)) {
+                    word.get(w).add(temp);
+                }else {
+                    word.put(w,new ArrayList<>());
+                    word.get(w).add(temp);
+                }
+            }
+        }
+    }
 }
--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Run {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        ChatBot b = new ChatBot();
+        System.out.println(b.welcomeMessage());
+        while(true) {
+            System.out.println(b.send(in.nextLine()));
+        }
     }
 }
