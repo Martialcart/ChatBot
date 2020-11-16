9ec4329b2755be9d45a7889aeefc5160ca7579a8
Jan_Olav_Berg <janolav91@hotmail.com>
2020-11-16 16:37:20 +0100

    added test names for Wordlink

---
+++ src/main/java/WordLink.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class WordLink {
+    private HashMap<String, ArrayList<Integer>> word = new HashMap<>();
+    private ArrayList<String> sentence = new ArrayList<>();
+}
---
+++ src/test/java/WordLinkTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WordLinkTest {
+
+    private WordLink wl;
+
+    @BeforeEach
+    public void setup() {
+        wl = new WordLink();
+    }
+
+    @Test
+    public void addWordTest(){
+
+    }
+
+    @Test
+    public void noWordDuplicates(){
+
+    }
+
+    @Test
+    public void noSentenceDuplicates(){}
+
+    @Test
+    public void noLinkDuplicates(){
+
+    }
+    @Test
+    public void addLinkTest() {
+
+
+    }
+
+    @Test
+    public void linkExistingWords() {
+
+    }
+}
