61b1d6700be471f1c2db249bd65e88631559ae88
Jan_Olav_Berg <janolav91@hotmail.com>
2020-11-16 16:45:19 +0100

    added documentation to WordLink

--- src/main/java/WordLink.java
+++ src/main/java/WordLink.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-
+/**
+ *A two way HashMap
+ * a word key can opens several sentences, and opposite.
+ */
 public class WordLink {
     private HashMap<String, ArrayList<Integer>> word = new HashMap<>();
     private ArrayList<String> sentence = new ArrayList<>();
