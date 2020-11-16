9ca080dc9cf9d85ee9427cbb681755a339cc03de
Jan_Olav_Berg <janolav91@hotmail.com>
2020-11-16 16:57:38 +0100

    corrected typo in wordLink documentation

--- src/main/java/WordLink.java
+++ src/main/java/WordLink.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 /**
  *A two way HashMap
- * a word key can opens several sentences, and opposite.
+ * a word key can open several sentences, and opposite.
  */
 public class WordLink {
     private HashMap<String, ArrayList<Integer>> word = new HashMap<>();
